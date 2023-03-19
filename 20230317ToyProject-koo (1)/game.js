e964b1f502b98ab7358cf9e1ac4c452efd176762
jiwoog0 <nikejin0920@gmail.com>
2023-03-19 22:06:13 +0900

    프로필 삽입

--- 20230317ToyProject-koo (1)/game.js
+++ 20230317ToyProject-koo (1)/game.js
@@ -3,20 +3,30 @@
 let player = [1, 120, 15, 5, 0];
 
 // 몬스터 생성 함수
-function createmonster(name, hp, atk, def, gift){
+function createmonster(name, hp, atk, def, gift, imageUrl){
     this.name = name;
     this.hp = hp;
     this.atk = atk;
     this.def = def;
     this.gift = gift;
+    this.imageUrl = imageUrl;
 }
 
+// 몬스터 이미지
+let monsterProfile = [
+    "./profile_image/monster1.png",
+    "./profile_image/monster2.png",
+    "./profile_image/monster3.png",
+    "./profile_image/monster4.png",
+    "./profile_image/monster5.png"
+]
+
 // 몬스터 정보
-let monster1 = new createmonster("buzz", 100, 10, 2, 10);
-let monster2 = new createmonster("alien", 150, 5, 1, 5);
-let monster3 = new createmonster("would you", 150, 10, 4, 15);
-let monster4 = new createmonster("trash", 80, 20, 5, 20);
-let monster5 = new createmonster("rabbit", 100, 10, 2, 10);
+let monster1 = new createmonster("buzz", 100, 10, 2, 10, monsterProfile[0]);
+let monster2 = new createmonster("alien", 150, 5, 1, 5, monsterProfile[1]);
+let monster3 = new createmonster("would you", 150, 10, 4, 15, monsterProfile[2]);
+let monster4 = new createmonster("trash", 80, 20, 5, 20, monsterProfile[3]);
+let monster5 = new createmonster("rabbit", 100, 10, 2, 10, monsterProfile[4]);
 
 // 몬스터 배열
 let monster = [monster1, monster2, monster3, monster4, monster5];
@@ -111,11 +121,24 @@ function faceToMonster(){
     let actionBtn = document.querySelector(".actionBtnGroup");
     actionBtn.classList.add("active");  
 
+    let attackRes = document.querySelector(".attackResult");
+    attackRes.classList.add("active");  
+    
+    // 전투 창 초기화
+    let playTurn = document.querySelector(".playerTurn");
+    let monTurn = document.querySelector(".monsterTurn");
+
+    playTurn.innerHTML = ``;
+    monTurn.innerHTML = ``;
+
     // 몬스터 생성    
     let monname = document.querySelector(".mstname");
     let monhp = document.querySelector(".msthp");
     let monatk = document.querySelector(".mstatk");
     let mondef = document.querySelector(".mstdef");
+
+    let monsterphoto = document.querySelector(".monsterphoto")
+    let monsterActive = document.querySelector(".monster.active")
     
     let random = Math.floor(Math.random() * monster.length);
     
@@ -127,6 +150,8 @@ function faceToMonster(){
     monhp.innerHTML = `${monsterSelect.hp} : 체력`;
     monatk.innerHTML = `${monsterSelect.atk} : 공격력`;
     mondef.innerHTML = `${monsterSelect.def} : 방어력`; 
+    monsterphoto.style.background = `url(${monsterSelect.imageUrl}) no-repeat center/100%` 
+    monsterActive.style.background = `url(${monsterSelect.imageUrl}) no-repeat center/100%`
     
     // 플레이어 정보 표기
     let playername = document.querySelector(".name");
@@ -140,14 +165,20 @@ function faceToMonster(){
     playerdef.innerHTML = `방어력 : ${player[3]}`;
 }
 
+// 전투 진행
 function fight(){
     let count = 1 ;
     let monhp = document.querySelector(".msthp");
     let playerhp = document.querySelector(".hp");
-    text = "";
+    text = "";  // 프로필 문구 초기화
 
+    // 크리티컬 설정 및 추가 문구
     let playerCri = Math.floor(Math.random() * 10);
     let monCri = Math.floor(Math.random() * 10);
+    playAtkText = "";  // 플레이어 공격 문구 초기화
+    monAtkText = "";  // 몬스터 공격 문구 초기화
+    playDemage = "";  // 플레이어 받은 데미지 문구 초기화
+    monDemage = "";   // 몬스터 받은 데미지 문구 초기화
 
     console.log(playerCri);
     console.log(monCri);
@@ -156,23 +187,39 @@ function fight(){
         if(count % 2 == 1){
             if(playerCri >= 5){
                 monsterSelect.hp = monsterSelect.hp - player[2] * 2 + monsterSelect.def;
+                playAtkText = `크리티컬 적용!<br> ${player[2] * 2}`;
+                monDemage = `${player[2] * 2 - monsterSelect.def}`;
             }else{
                 monsterSelect.hp = monsterSelect.hp - player[2] + monsterSelect.def;
+                playAtkText = `${player[2]}`;
+                monDemage = `${player[2] - monsterSelect.def}`;
             }
         }else{
             if(monCri >= 5){
-                player[1] = player[1] - monsterSelect.atk * 2 + player[3];                
+                player[1] = player[1] - monsterSelect.atk * 2 + player[3];      
+                monAtkText = `크리티컬 적용!<br> ${monsterSelect.atk * 2}`;     
+                playDemage = `${monsterSelect.atk * 2 - player[3]}`;     
             }else{
                 player[1] = player[1] - monsterSelect.atk + player[3];
+                monAtkText = `${monsterSelect.atk}`;  
+                playDemage = `${monsterSelect.atk - player[3]}`;
             }
         }
         
         count++;        
     }
-
+    
     console.log(player);
     console.log(monsterSelect);
 
+    // 전투 회당 결과창 호출
+    let playTurn = document.querySelector(".playerTurn");
+    let monTurn = document.querySelector(".monsterTurn");
+
+    playTurn.innerHTML = `플레이어 공격!<br> ${playAtkText}으로 공격!<br> 몬스터 체력 ${monDemage} 감소!`;
+    monTurn.innerHTML = `몬스터 공격!<br> ${monAtkText}으로 공격!<br> 플레이어 체력 ${playDemage} 감소!`;
+
+    // 전투 결과 프로필 적용
     text = `${monsterSelect.hp} : 체력`;
     monhp.innerHTML = `${text}`;
     playerhp.innerHTML = `체력 : ${player[1]}`;
@@ -182,6 +229,7 @@ function fight(){
     }
 }
 
+// 체력이 줄었을 때 결과 비교 및 보상
 function afterFight(){
     if(player[0] <= 0){        
         alert(`패배하셨습니다. 마을로 돌아갑니다...`)
@@ -199,19 +247,16 @@ function afterFight(){
     player[1] = playerInit[1];
     
     // 몬스터 초기화 배열
-    let monInit1 = new createmonster("buzz", 100, 10, 2, 10);
-    let monInit2 = new createmonster("alien", 150, 5, 1, 5);
-    let monInit3 = new createmonster("would you", 150, 10, 4, 15);
-    let monInit4 = new createmonster("trash", 80, 20, 5, 20);
-    let monInit5 = new createmonster("rabbit", 100, 10, 2, 10);
+    let monInit1 = new createmonster("buzz", 100, 10, 2, 10, monsterProfile[0]);
+    let monInit2 = new createmonster("alien", 150, 5, 1, 5, monsterProfile[1]);
+    let monInit3 = new createmonster("would you", 150, 10, 4, 15, monsterProfile[2]);
+    let monInit4 = new createmonster("would you", 150, 10, 4, 15, monsterProfile[3]);
+    let monInit5 = new createmonster("would you", 150, 10, 4, 15, monsterProfile[4]);
     let monInit = [monInit1, monInit2, monInit3, monInit4, monInit5];
     
     // 몬스터 리셋
     monster[monRandom] = monInit[monRandom];
 
-    console.log(monster[monRandom]);
-    console.log(monInit[monRandom]);
-        
     // 메인 화면과 전투 화면 스위칭 및 프로필 오프
     let gamePlaying = document.querySelector(".gameArea");
     gamePlaying.classList.remove("active");
@@ -233,4 +278,7 @@ function afterFight(){
     
     let actionBtn = document.querySelector(".actionBtnGroup");
     actionBtn.classList.remove("active");  
+
+    let attackRes = document.querySelector(".attackResult");
+    attackRes.classList.remove("active");  
 }
